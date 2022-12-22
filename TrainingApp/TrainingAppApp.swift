57f3e5a0fab25c970fdc9b5a4a2b20c027265f78
Tom Montgomery <montgomery3681@gmail.com>
2022-12-21 22:56:45 -0700

    Initial Commit

---
+++ TrainingApp/TrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingAppApp.swift
+//  TrainingApp
+//
+//  Created by tom montgomery on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct TrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
