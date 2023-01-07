e76b3deab9ba6942f2ada06c2145232c64e9da8e
Tom Montgomery <montgomery3681@gmail.com>
2023-01-07 14:58:49 -0700

    - add HomeView cards for courses and tests - extract to subview

--- TrainingApp/Views/HomeView.swift
+++ TrainingApp/Views/HomeView.swift
@@ -14,13 +14,54 @@ struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        // Put the whole thing in a NavView
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("What do you want to do today?")
+                    .padding(.leading, 20)
+                
+                // List of cards is a scrollview
+                ScrollView {
+                    
+                    // Load cards only as we need them
+                    LazyVStack {
+                        
+                        // create each card with a ForEach specific to view creation
+                        ForEach (model.modules) { module in
+                            
+                            // wrap in VS to get a little extra spacing between cards
+                            VStack(spacing:20) {
+                                // Learning Card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                // Test Card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+
+                                
+                            }
+
+                        }
+                        
+                    }
+                    .padding()
+                    // add padding on the LazyV to accomodate .aspectRatio()
+                }
+            }
+            .navigationTitle("Get Started")
+            // Add the .navTitle on the VStack
+        }
+
+        
+
+
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
+        // must add the EO on the preview with an instance of the ContentModel in order for things to render
     }
 }
---
+++ TrainingApp/Views/HomeViewRow.swift
@@ -0,0 +1,88 @@
+//
+//  HomeViewRow.swift
+//  TrainingApp
+//
+//  Created by tom montgomery on 1/6/23.
+//
+
+import SwiftUI
+
+struct HomeViewRow: View {
+    
+    // pass everything in
+    var image: String
+    var title: String
+    var description: String
+    var count: String
+    var time: String
+    
+    var body: some View {
+
+        // each card will sit atop a rectangle
+        ZStack {
+            // nice styled background
+            Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+            // use aspectRatio instead of .frame in this case to optimize for different screen sizes
+                //.frame(width: 335, height: 175)
+            
+            // all of the elements on top of the rectangle
+            HStack {
+                
+                // Course Image
+                Image(image)
+                    .resizable()
+                    .frame(width: 116, height: 116)
+                    .clipShape(Circle())
+                // yay clipShape!
+                Spacer()
+                VStack(alignment: .leading, spacing: 10){
+                    // left align the whole VStack
+                    // course name  (Headline)
+                    Text(title)
+                        .bold()
+                    // course description
+                    Text(description)
+                        .padding(.bottom, 20)
+                        .font(.caption)
+                    // padding to create the necessary space in addition to the spacing on the parent VStack
+                    // Icons - in the VStack
+                    HStack {
+                        // # of lessons
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(count)
+                            //.font(.caption)  was too big, it wrapped
+                            .font(Font.system(size: 10))
+                        Spacer()
+                        // time (duration)
+                        Image(systemName: "clock")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                        Text(time)
+                            .font(Font.system(size: 10))
+                            
+                    }
+                    
+                }
+                .padding(.leading, 20)
+                
+                
+            }
+            .padding(.horizontal, 20)
+            // Add padding to parent HStack to push back on the Spacer() a bit
+            
+        }
+        
+    }
+}
+
+struct HomeViewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewRow(image: "swift", title: "Learn Swift", description: "An intro to Swift Programming", count: "10 Lessons", time: "3 Hours")
+    }
+}
